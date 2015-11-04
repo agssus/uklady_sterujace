884a25534c0aeebfa653e9fa86a80c7a53cb32f5
agssus <agssus11@gmail.com>
2015-11-04 21:30:14 +0100

    Update and rename zajecia11.04 to GccApplication1.c

---
+++ GccApplication1.c
@@ -0,0 +1,33 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 2015-11-04 16:15:14
+ *  Author: Administrator
+ */ 
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+	DDRA=0xFF;
+	PORTA=0x00;
+    while(1)
+    {
+		for(int i=0; i<8; i++){
+			PORTA = 1 << i;
+			_delay_ms(800);
+		}
+		_delay_ms(800);
+		
+		for(int i=7; i>=0; i--){
+			PORTA = 1 << i;
+			_delay_ms(800);
+		}
+		_delay_ms(800);
+		
+    }
+}
+
+
