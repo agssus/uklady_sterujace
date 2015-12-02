7db94ad3e21ee3681b21d04f8a9b906818d4efd8
agssus <agssus11@gmail.com>
2015-12-02 17:34:56 +0100

    Create cw4zad1.c

---
+++ cw4zad1.c
@@ -0,0 +1,91 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+int digits[10] = {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8,0x80, 0x90};
+int temp_time_counter = 0;
+int time = 0;
+
+int column1 = ~0xF1;
+int column2 = ~0xF2;
+int column3 = ~0xF4;
+int column4 = ~0xF8;
+
+int column_chooser = 0;
+
+ISR(TIMER0_COMP_vect){
+	
+	//if(column_chooser == 0){
+		//PORTB = column1;
+		//PORTA = digits[time % 10];
+	//}
+	//else if (column_chooser == 1){
+		//PORTB = column2;
+		//PORTA = digits[(time/10) % 10];
+	//}
+	//else if(column_chooser == 2){
+		//PORTB = column3;
+		//PORTA = digits[(time/100) % 10];
+	//}
+	//else if (column_chooser == 3){
+		//PORTB = column4;
+		//PORTA = digits[(time/1000) % 10];
+	//}
+	//
+	//column_chooser = (column_chooser + 1) % 4;
+	//
+	//temp_time_counter++;
+	//if(temp_time_counter >= 1000){
+		//time++;
+		//temp_time_counter = 0;
+	//}
+	//if ((PIND) == (0xE0))
+	//{
+				//PORTB = column1;
+				//PORTA = digits[1];
+		//
+	//}
+}
+
+int main(void)
+{
+	TCCR0 = (1 << WGM01) | (0 << WGM00) | (1 << CS00) | (1 << CS01) | (0 << CS02);
+	OCR0 = 250;
+	TIMSK |= 1 << OCIE0;
+	
+	sei();
+	
+	DDRA = 0xFF;
+	PORTA = 0x00;
+	
+	DDRB = 0xFF;
+	PORTB = ~0xF8;
+	
+	DDRD = 0xF0;
+	PORTD = 0x0F;
+	//DDRD = 0xF;
+
+		
+	while(1)
+	{
+		if (((~PIND) & (1 << 0)) && ((~PIND) & (0 << 4))  )
+		{
+			PORTA = digits[0];
+		}
+		if ((~PIND) & (1 << 1))
+		{
+			PORTA = digits[1];
+		}
+		if ((~PIND) & (1 << 2))
+		{
+			PORTA = digits[2];
+		}
+		if ((~PIND) & (1 << 3))
+		{
+			PORTA = digits[3];
+		}
+					
+		//PORTA = PIND;
+	}
+}
